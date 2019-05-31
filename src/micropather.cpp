76fa102a3deb8b4b0f01944c4e0157f9c19c3893
brinkqiang <brink.qiang@gmail.com>
2019-05-31 15:54:43 +0800

    add

--- src/micropather.cpp
+++ src/micropather.cpp
@@ -1,27 +1,3 @@
-/*
-Copyright (c) 2000-2009 Lee Thomason (www.grinninglizard.com)
-
-Grinning Lizard Utilities.
-
-This software is provided 'as-is', without any express or implied 
-warranty. In no event will the authors be held liable for any 
-damages arising from the use of this software.
-
-Permission is granted to anyone to use this software for any 
-purpose, including commercial applications, and to alter it and 
-redistribute it freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must 
-not claim that you wrote the original software. If you use this 
-software in a product, an acknowledgment in the product documentation 
-would be appreciated but is not required.
-
-2. Altered source versions must be plainly marked as such, and 
-must not be misrepresented as being the original software.
-
-3. This notice may not be removed or altered from any source 
-distribution.
-*/
 
 #ifdef _MSC_VER
 #pragma warning( disable : 4786 )	// Debugger truncating names.
@@ -30,15 +6,6 @@ distribution.
 
 #include <memory.h>
 #include <stdio.h>
-
-//#define DEBUG_PATH
-//#define DEBUG_PATH_DEEP
-//#define TRACK_COLLISION
-//#define DEBUG_CACHING
-
-#ifdef DEBUG_CACHING
-#include "../grinliz/gldebug.h"
-#endif
 
 #include "micropather.h"
 
